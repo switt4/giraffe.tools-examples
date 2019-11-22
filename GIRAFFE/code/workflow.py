daffed2759d4c70d371e0c6c5109aaa6e479c898
SW <39967319+switt4@users.noreply.github.com>
2019-11-22 10:03:17 -0500

    update base directory variable names

--- GIRAFFE/code/workflow.py
+++ GIRAFFE/code/workflow.py
@@ -5,25 +5,20 @@ import sys
 import nipype
 import nipype.pipeline as pe
 
-import nipype.interfaces.fsl as fsl
 import nipype.interfaces.io as io
+import nipype.interfaces.fsl as fsl
 import nipype.interfaces.afni as afni
 import nipype.algorithms.confounds as confounds
 
-#Wraps the executable command ``mcflirt``.
-fsl_mcflirt = pe.MapNode(interface = fsl.MCFLIRT(), name='fsl_mcflirt', iterfield = ['in_file'])
-
 #Flexibly collect data from disk to feed into workflows.
 io_select_files = pe.Node(io.SelectFiles(templates={'anat':'sub-{subID}/anat/sub-{subID}_*_T1w.nii.gz','func':'sub-{subID}/func/sub-{subID}_*_bold.nii.gz'}), name='io_select_files', iterfield = ['subID'])
-io_select_files.inputs.base_directory = bidsDir
+io_select_files.inputs.base_directory = bids_dir
 io_select_files.inputs.anat = 'sub-{subID}/anat/sub-{subID}_*_T1w.nii.gz'
 io_select_files.inputs.func = 'sub-{subID}/func/sub-{subID}_*_bold.nii.gz'
 io_select_files.iterables = [('subID', subID)]
 
-#Generic datasink module to store structured outputs
-io_data_sink = pe.MapNode(interface = io.DataSink(), name='io_data_sink', iterfield = ['smoothedEPI', 'framewiseDisplacement', 'mcflirtPar', 'aCompCorComponents'])
-io_data_sink.inputs.base_directory = outDir
-io_data_sink.inputs.parameterization = True
+#Wraps the executable command ``mcflirt``.
+fsl_mcflirt = pe.MapNode(interface = fsl.MCFLIRT(), name='fsl_mcflirt', iterfield = ['in_file'])
 
 #Wraps the executable command ``bet``.
 fsl_bet = pe.MapNode(interface = fsl.BET(), name='fsl_bet', iterfield = ['in_file'])
@@ -52,13 +47,18 @@ afni_blur_to_fwhm.inputs.fwhm = 6
 confounds_framewise_displacement = pe.MapNode(interface = confounds.FramewiseDisplacement(), name='confounds_framewise_displacement', iterfield = ['in_file'])
 confounds_framewise_displacement.inputs.parameter_source = 'FSL'
 
+#Wraps the executable command ``fast``.
+fsl_fast = pe.MapNode(interface = fsl.FAST(), name='fsl_fast', iterfield = ['in_files'])
+fsl_fast.inputs.number_classes = 3
+
 #Anatomical compcor: for inputs and outputs, see CompCor.
 confounds_acomp_cor = pe.MapNode(interface = confounds.ACompCor(), name='confounds_acomp_cor', iterfield = ['realigned_file', 'mask_files'])
 confounds_acomp_cor.inputs.merge_method = 'union'
 
-#Wraps the executable command ``fast``.
-fsl_fast = pe.MapNode(interface = fsl.FAST(), name='fsl_fast', iterfield = ['in_files'])
-fsl_fast.inputs.number_classes = 3
+#Generic datasink module to store structured outputs
+io_data_sink = pe.MapNode(interface = io.DataSink(), name='io_data_sink', iterfield = ['smoothedEPI', 'framewiseDisplacement', 'mcflirtPar', 'aCompCorComponents'])
+io_data_sink.inputs.base_directory = out_dir
+io_data_sink.inputs.parameterization = True
 
 #Create a workflow to connect all those nodes
 analysisflow = nipype.Workflow('MyWorkflow')
